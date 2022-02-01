d81875e543c9939a782109c2300ebb387b74c8c5
Isabella X. Risso <ixrisso@gmail.com>
2022-02-01 12:26:07 -0300

    its working

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
-import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Kitchen from "./components/Kitchen/Kitchen";
-import Waiter from "./components/Waiter/Waiter";
-import Landing from "./components/Landing/Landing";
-import Menu from "./components/Menu/Menu";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <div className="App">
-          <h1>todo ok</h1>
-          <div>
-            <Link to="/">Landing</Link>
-          </div>
-          <hr />
-          <Switch>
-            <Route path="/waiter">
-              <Waiter />
-            </Route>
-            <Route path="/kitchen">
-              <Kitchen />
-            </Route>
-            <Route path="/" exact>
-              <Landing />
-            </Route>
-          </Switch>
-
-          <div>
-            <Menu />
-          </div>
-        </div>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
